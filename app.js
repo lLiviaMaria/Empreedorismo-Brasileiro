190939e9f3cd5d2875d8a7bcf18c826450a39eb2
lLiviaMaria <123980118+lLiviaMaria@users.noreply.github.com>
2024-09-09 19:54:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+  
+    if(!campoPesquisa){
+        section.innerHTML = `
+       <div class="item-resultado">
+         <h2>nada foi encontrado</h2>
+         <p> Você precisa digitar algo para iniciar a pesquisa </p>
+         </div>
+       
+       `
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase( )
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao ="";
+    let tags = "";
+;
+    // Itera sobre cada dado da pesquisa
+    for(let dado of dados){
+        titulo= dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+         
+        // se titulo includes campoPesquisa
+          if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+      
+        // Cria um novo elemento div para cada resultado 
+        resultados += `
+        <div class="item-resultado"> 
+            <h2> ${dado.titulo} </h2>
+            <p class="descricao-meta">${dado.descricao} </p>
+            <a href=${dado.linklinkedin} target="_blank"> Site Oficial </a> <br>        
+            <a href=${dado.linksite} target="_blank">Linkedin </a>
+        </div>
+    `;
+     }
+    }
+    if (!resultados) {
+        resultados =`<div class="item-resultado">
+         <h2>Nada foi encontrado</h2>
+         <p>Empresa não se encontra em nosso banco ou não é uma empresa brasileira</p> 
+         </div>
+         `
+}
+    // Insere os resultados gerados na seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,68 @@
+let dados = [
+    {
+      titulo: "Petrobras",
+      descricao: "A Petrobras é uma empresa brasileira de energia, atuando na exploração, produção, refino, transporte, comercialização e petroquímica. É uma das maiores empresas de energia do mundo.",
+      linksite: "https://www.petrobras.com.br/",
+      linklinkedin: "https://www.linkedin.com/company/petrobras/",
+      tags: "petróleo gás energia combustível exploração produção refino"
+    },
+    {
+      titulo: "Ambev",
+      descricao: "A Ambev é uma empresa brasileira de bebidas, líder no mercado cervejeiro. Possui um vasto portfólio de marcas, incluindo cervejas, refrigerantes e outras bebidas.",
+      linksite: "https://www.ambev.com.br/",
+      linklinkedin: "https://www.linkedin.com/company/ambev/",
+      tags: "cervejaria fabrica industria bebida refrigerante"
+    },
+    {
+      titulo: "Rede D'Or",
+      descricao: "A Rede D'Or São Luiz é uma das maiores redes de hospitais privados do Brasil, oferecendo serviços de alta complexidade em diversas especialidades médicas.",
+      linksite: "https://www.redor.org.br/",
+      linklinkedin: "https://www.linkedin.com/company/rede-dor-sao-luiz/",
+      tags: "hospital saúde medicina saúde privada hospitalização"
+    },
+    {
+      titulo: "Itaú",
+      descricao: "O Itaú Unibanco é um dos maiores bancos privados da América Latina, oferecendo uma ampla gama de produtos e serviços financeiros.",
+      linksite: "https://www.itau.com.br/",
+      linklinkedin: "https://www.linkedin.com/company/itau/",
+      tags: "banco finanças investimento crédito banco digital"
+    },
+    {
+      titulo: "Boca Rosa Beauty",
+      descricao: "Boca Rosa Beauty é a marca de cosméticos criada pela influenciadora digital brasileira Bianca Andrade, mais conhecida como Boca Rosa. A marca oferece uma ampla variedade de produtos de beleza.",
+      linksite: "https://www.bocarosabeauty.com.br/",
+      linklinkedin: "https://www.linkedin.com/company/boca-rosa-company/",
+      tags: "cosméticos beleza maquiagem skincare influenciadora"
+    },
+    {
+      titulo: "Stone",
+      descricao: "A Stone é uma empresa brasileira de tecnologia financeira que oferece soluções de pagamento para empresas de todos os portes.",
+      linksite: "https://www.stone.com.br/",
+      linklinkedin: "https://www.linkedin.com/company/stone-pagamentos/",
+      tags: "fintech pagamento financeiro tecnologia financeira"
+    },
+    {
+        titulo: "iFood",
+        descricao: "O iFood é uma plataforma brasileira que conecta restaurantes e entregadores, facilitando a entrega de comida.",
+        linksite: "https://www.ifood.com.br/",
+        linklinkedin: "https://www.linkedin.com/company/ifood-/",
+        tags: "delivery foodtech tecnologia"
+      },
+      ,
+  {
+    titulo: "Magazine Luiza",
+    descricao: "O Magazine Luiza é uma das maiores redes varejistas do Brasil, oferecendo uma ampla variedade de produtos e serviços.",
+    linksite: "https://www.magazineluiza.com.br/",
+    linklinkedin: "https://www.linkedin.com/company/magazine-luiza/",
+    tags: "varejo e-commerce eletrodomésticos moda"
+  },
+  ,
+  {
+    titulo: "Vivo",
+    descricao: "A Vivo é uma das maiores empresas de telecomunicações do Brasil, oferecendo serviços de telefonia móvel, fixa e internet.",
+    linksite: "https://www.vivo.com.br/",
+    linklinkedin: "https://www.linkedin.com/company/vivo-telefonicabr/",
+    tags: "telecomunicações telefonia celular internet"
+  },
+
+  ];
